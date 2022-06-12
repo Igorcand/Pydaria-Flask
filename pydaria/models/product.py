ea05fa16028a0a68c83aa8c9af0e6316e7617057
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-12 10:02:46 -0300

    added folder models

---
+++ pydaria/models/product.py
@@ -0,0 +1,26 @@
+from ext.database import db
+#from sqlalchemy_serializer import SerializerMixin
+
+
+class Product(db.Model): #SerializerMixin
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    price = db.Column(db.Numeric())
+    description = db.Column(db.Text)
+
+
+    def __init__(self, id, name, price, description):
+        self.id = id 
+        self.name = name 
+        self.price = price
+        self.description = description
+
+
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price' : self.price,
+            'description' : self.description
+        }
+
