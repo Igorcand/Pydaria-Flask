a95ff596101f2c454470df680bcf460f969505b9
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-06-12 10:14:50 -0300

    change caller on init to specify folder resources

--- pydaria/blueprints/restapi/__init__.py
+++ pydaria/blueprints/restapi/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .resources import ProductResource, ProductItemResource
+#from .resources import ProductResource, ProductItemResource
+from .resources.product import ProductResource, ProductItemResource
+
 
 bp = Blueprint('restapi', __name__, url_prefix='/api/v1')
 api = Api(bp)
